0914ad1ce3bdd4dc9d5e9a703241f6d1282c1f24
Ashiqur Rahman <ashiquebd@gmail.com>
2016-11-10 17:48:03 +0600

    Demo popup
    Added CSS and JS for demo popup.

---
+++ demo-popup/js/popup.js
@@ -0,0 +1,8 @@
+/**
+ * Expressly popup script.
+ * Handles the popup and it's content related actions.
+ **/
+function xly_close_popup() {
+    var popup = document.getElementById("xly-demo-popup");
+    popup.remove(popup.selectedIndex);
+}
