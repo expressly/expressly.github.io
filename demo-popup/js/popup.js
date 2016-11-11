b96d0037ba302b34f3bf92513416267337fd7bf5
Ashiqur Rahman <ashiquebd@gmail.com>
2016-11-11 16:18:27 +0600

    Fixed the hind popup issue with IE11 and small screen.

--- demo-popup/js/popup.js
+++ demo-popup/js/popup.js
@@ -2,7 +2,15 @@
  * Expressly popup script.
  * Handles the popup and it's content related actions.
  **/
+
+window.onload = function() {
+    var popup = document.getElementById("xly-demo-popup");
+    popup.parentNode.removeChild(popup);
+    popup.style.display = 'block';
+    document.body.appendChild(popup);
+};
+
 function xly_close_popup() {
     var popup = document.getElementById("xly-demo-popup");
-    popup.remove(popup.selectedIndex);
+    popup.parentNode.removeChild(popup);
 }
