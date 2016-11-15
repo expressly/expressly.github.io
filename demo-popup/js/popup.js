958c9a339752218c1536eae55ca63b05b97840f5
Ashiqur Rahman <ashiquebd@gmail.com>
2016-11-15 18:17:36 +0600

    Changed top position of the demo-hint popup. Added event to close popup when "Start Demo" is clicked.
    Issue: #13.

--- demo-popup/js/popup.js
+++ demo-popup/js/popup.js
@@ -8,9 +8,32 @@ window.onload = function() {
     popup.parentNode.removeChild(popup);
     popup.style.display = 'block';
     document.body.appendChild(popup);
+
+    /** Following line enables hiding the popup when clicked outside **/
+    //document.body.addEventListener('click', body_clicked);
 };
+
+function body_clicked(e) {
+    var target = e.target;
+    var popup = document.getElementById("xly-demo-popup");
+
+    if(target == undefined || popup == undefined) {
+        return;
+    }
+
+    if(target == popup || popup.contains(target)) {
+        return;
+    }
+
+    xly_close_popup();
+}
 
 function xly_close_popup() {
     var popup = document.getElementById("xly-demo-popup");
+
+    if(popup == undefined) {
+        return;
+    }
+
     popup.parentNode.removeChild(popup);
 }
