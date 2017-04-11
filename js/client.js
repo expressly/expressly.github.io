1483b6c8fd222cda49151961a8f62c88309c102a
jacubsmith <jakesmith1922@googlemail.com>
2017-04-11 18:04:15 +0100

    Changes to JS to autofill form and add fields in template

--- js/client.js
+++ js/client.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
     var cp_obj = CraftyPostcodeCreate();
     cp_obj.set("access_token", "3436f-5ccf3-93f86-02095"); // your token here
     cp_obj.set("result_elem_id", "crafty_postcode_result_display");
@@ -18,9 +17,21 @@
         }
     })();
 
-    $(function() {
+    jQuery(document).ready(function () {
+      var milliseconds = new Date().getTime();
+      jQuery('#xly-firstName input').val('Jake');
+      jQuery('#xly-lastName input').val('Smith');
+      jQuery('#xly-email input').val('Jake'+ milliseconds + '@email.com');
+      jQuery('#xly-phone input').val('07920599089');
+      jQuery('#xly-postcode input').val('Cf64 1AZ');
+      jQuery('#xly-address input').val('11 Church Avenue');
+      jQuery('#xly-town input').val('Penarth');
+    });
 
-    var form = $("#xly-submit-form");
+    jQuery(function() {
+
+    console.log("working");
+    var form = jQuery("#xly-submit-form");
 
     form.submit(function(event) {
 
@@ -30,41 +41,97 @@
 
         submitButton.val('Submitting...');
 
-        $.ajaxSetup({
-            xhrFields: {
-                withCredentials: true
-            }});
+        jQuery.ajax({
+            type: "POST",
+            url: "/my-account/",
+            data: {
+              email: jQuery('#xly-email input').val(),
+              password: "asd234fdgd!!!",
+              'woocommerce-register-nonce': "aa96605c2c",
+              register: "Register"
+            },
+            success: function(output){
+                if (xlyRegistrationSuccessful(output)) {
+                  xlyAddBillingDetails();
+                  xlySendMigrationSuccess();
 
-        $.post( "http://sunflowerbaby.uk/my-account/", form.serialize())
-            .done(function(data) {
-                form.find('.form-item').removeClass('error');
-                form.find('.xly-error').removeClass('field-error');
-                form.find('.xly-error').hide();
+                };
+            }
+        });
 
-                if (!data.success) {
-                    $.each( data.errors.fieldErrors, function( key, value ) {
-                        var fieldDiv = form.find('#xly-' + key);
-                        var fieldError = fieldDiv.find('.xly-error');
-                        fieldDiv.addClass('error');
-                        fieldError.text(value);
-                        fieldError.show();
-                        fieldError.addClass('field-error');
-                    });
+        function xlyRegistrationSuccessful(output) {
+            return true;
+        };
 
-                    if (data.errors.globalError) {
-                        var globalError = form.find('#xly-globalError');
-                        globalError.text(data.errors.globalError);
-                        if (data.errors.globalErrorHelp) {
-                            globalError.text(globalError.text() + '. ' + data.errors.globalErrorHelp);
-                        }
-                        globalError.show();
-                        globalError.addClass('field-error');
-                        submitButton.val('OK');
-                    }
-                } else {
-                    window.location.replace("https://prod.expresslyapp.com/api/redirect/migration/{uuid}/failed");
-                }
-            });
+        function xlyAddBillingDetails() {
+          jQuery.ajax({
+              type: "POST",
+              url: "my-account/edit-address/billing/",
+              data: {
+                billing_first_name: jQuery('#xly-fullName input').val(),
+                billing_phone: jQuery('#xly-phone input').val(),
+                billing_postcode: jQuery('#xly-postcode input').val(),
+                billing_address_1: jQuery('#crafty_postcode_lookup_result_option1 select').val(),
+                '_wpnonce':'82e6f8cf7e',
+                save_address: 'Save Address'
+              },
+              success: function(output){
+              }
           });
+        }
+
+        function xlySendMigrationSuccess() {
+
+        }
+
+        function xlySendMigrationFailure() {
+
+        }
+
       });
-</script>
+    });
+
+
+
+
+
+
+
+
+
+    // jQuery.ajaxSetup({
+    //     xhrFields: {
+    //         withCredentials: true
+    //     }});
+    //
+    //     jQuery.post( "/my-account/", {
+    //     email:
+    //     }).done(function(data)
+    //     {
+    //         form.find('.form-item').removeClass('error');
+    //         form.find('.xly-error').removeClass('field-error');
+    //         form.find('.xly-error').hide();
+    //         if (!data.success) {
+    //             $.each( data.errors.fieldErrors, function( key, value ) {
+    //                 var fieldDiv = form.find('#xly-' + key);
+    //                 var fieldError = fieldDiv.find('.xly-error');
+    //                 fieldDiv.addClass('error');
+    //                 fieldError.text(value);
+    //                 fieldError.show();
+    //                 fieldError.addClass('field-error');
+    //             });
+    //
+    //             if (data.errors.globalError) {
+    //                 var globalError = form.find('#xly-globalError');
+    //                 globalError.text(data.errors.globalError);
+    //                 if (data.errors.globalErrorHelp) {
+    //                     globalError.text(globalError.text() + '. ' + data.errors.globalErrorHelp);
+    //                 }
+    //                 globalError.show();
+    //                 globalError.addClass('field-error');
+    //                 submitButton.val('OK');
+    //             }
+    //         } else {
+    //             window.location.replace("https://prod.expresslyapp.com/api/redirect/migration/{uuid}/failed");
+    //         }
+    //     });
