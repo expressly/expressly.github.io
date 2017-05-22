728d093125d0e60f9ea0f607c0afa7c9286e25cb
Marc <marc.g.smith@gmail.com>
2017-05-22 21:37:37 +0100

    Submit button not properly reference

--- lightbox/lightbox.form.js
+++ lightbox/lightbox.form.js
@@ -145,7 +145,7 @@ var xlyr = xlyr || {
         },
 
         expresslyContinue: function(event) {
-            submitButton.style.display = "none";
+            xlyr.submitButton.style.display = "none";
             var closeButton = jQuery('.xly-decline')[0];
             jQuery(closeButton).css({'display': 'none'});
             var loader = jQuery('.xly-loader');
