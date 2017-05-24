92fa1b862de7601113bd19dad948a1cda4ee110a
Marc <marc.g.smith@gmail.com>
2017-05-24 16:50:48 +0100

    Updated invalid date of birth message

--- lightbox/lightbox.form.js
+++ lightbox/lightbox.form.js
@@ -131,7 +131,7 @@ var xlyr = xlyr || {
             var re = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
             if (!re.test(dob)) {
                 this.error.css({'display': 'block', 'margin-bottom': '5px', 'border-radius': '5px'});
-                this.error.text('Invalid date of birth');
+                this.error.text('Please insert date as dd/mm/yyyy');
                 this.dobField.css({'border': '1px solid red'});
                 return false;
             }
@@ -140,7 +140,7 @@ var xlyr = xlyr || {
             var dobDate = new Date(xlyr.xlyFormattedDate(dob));
             if (isNaN(dobDate) || dobDate.getFullYear() < 1900) {
                 this.error.css({'display': 'block', 'margin-bottom': '5px', 'border-radius': '5px'});
-                this.error.text('Invalid date of birth');
+                this.error.text('Please insert date as dd/mm/yyyy');
                 this.dobField.css({'border': '1px solid red'});
                 return false;
             }
