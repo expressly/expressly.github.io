2e5d86ea44dd60ff4c354984fb665405ec75c91b
Marc <marc.g.smith@gmail.com>
2017-07-17 17:25:10 +0100

    Handle cross protocol in older IE browsers

--- adserving/adserver.js
+++ adserving/adserver.js
@@ -1,70 +1,91 @@
-var xly = xly || {
-        callExpressly: function (path, callback, el, data) {
+var xly = xly || (function () {
+        if (!parent.document.xlyQueue) {
+            parent.document.xlyQueue = [];
+        }
+
+        var queue = parent.document.xlyQueue;
+        function push(path, el, data) {
+            queue.push({
+                path: path,
+                el: el,
+                data: data
+            });
+        }
+
+        function pop() {
+            parent.document.xlyBusy = false;
+            if (queue && queue.length > 0) {
+                var request = queue.shift();
+                call(request.path, request.el, request.data);
+            }
+        }
+
+        function call(path, el, data) {
             if (parent.document.xlyBusy) {
-                if (!parent.document.xlyQueue) {
-                    parent.document.xlyQueue = [];
-                }
-
-                parent.document.xlyQueue.push({
-                    path: path,
-                    callback: callback,
-                    el: el,
-                    data: data
-                });
-
+                push(path, el, data);
                 return;
             }
+
             parent.document.xlyBusy = true;
 
             var xhr;
+            var protocol = 'https:';
+            if (typeof XDomainRequest !== 'undefined') {
+                xhr = new XDomainRequest();
+                xhr.onload = function () {
+                    pop();
+                    render(JSON.parse(xhr.responseText), el);
+                };
+                xhr.onerror = pop;
+                xhr.setRequestHeader = function () {
+                };
+                protocol = window.location.protocol;
+            } else {
+                if (typeof XMLHttpRequest !== 'undefined') {
+                    xhr = new XMLHttpRequest();
+                }
+                else {
+                    var versions = ["MSXML2.XmlHttp.5.0",
+                        "MSXML2.XmlHttp.4.0",
+                        "MSXML2.XmlHttp.3.0",
+                        "MSXML2.XmlHttp.2.0",
+                        "Microsoft.XmlHttp"];
 
-            if (typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();
-            else {
-                var versions = ["MSXML2.XmlHttp.5.0",
-                    "MSXML2.XmlHttp.4.0",
-                    "MSXML2.XmlHttp.3.0",
-                    "MSXML2.XmlHttp.2.0",
-                    "Microsoft.XmlHttp"];
-
-                for (var i = 0, len = versions.length; i < len; i++) {
-                    try {
-                        xhr = new ActiveXObject(versions[i]);
-                        break;
-                    }
-                    catch (e) {
+                    for (var i = 0, len = versions.length; i < len; i++) {
+                        try {
+                            xhr = new ActiveXObject(versions[i]);
+                            break;
+                        }
+                        catch (e) {
+                        }
                     }
                 }
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState < 4) {
+                        return;
+                    }
+
+                    pop();
+
+                    if (xhr.status !== 200) {
+                        return;
+                    }
+
+                    if (xhr.readyState === 4) {
+                        render(JSON.parse(xhr.responseText), el);
+                    }
+                };
             }
 
-            xhr.onreadystatechange = ensureReadiness;
 
-            function ensureReadiness() {
-                if (xhr.readyState < 4) {
-                    return;
-                }
-
-                parent.document.xlyBusy = false;
-                if (parent.document.xlyQueue && parent.document.xlyQueue.length > 0) {
-                    var request = parent.document.xlyQueue.shift();
-                    xly.callExpressly(request.path, request.callback, request.el, request.data);
-                }
-
-                if (xhr.status !== 200) {
-                    return;
-                }
-
-                if (xhr.readyState === 4) {
-                    callback(JSON.parse(xhr.responseText), el);
-                }
-            }
-
-            xhr.open('POST', "https://prod.expresslyapp.com/api/adserver/banner" + path, true);
+            xhr.open('POST', protocol + "//prod.expresslyapp.com/api/adserver/banner" + path, true);
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.withCredentials = true;
             xhr.send(JSON.stringify(data));
-        },
+        }
 
-        getXlyAttr: function (attribute) {
+        function getXlyAttr(attribute) {
             var metas = parent.document.getElementsByTagName('meta');
             for (var i = 0; i < metas.length; i++) {
                 if (metas[i].getAttribute("property") === ("xly:" + attribute)) {
@@ -72,25 +93,25 @@ var xly = xly || {
                 }
             }
             return null;
-        },
+        }
 
-        getPpid: function () {
-            return this.getXlyAttr('ppid');
-        },
+        function getPpid() {
+            return getXlyAttr('ppid');
+        }
 
-        getMerchant: function () {
-            return 'undefined' === typeof xlyMerchant || xlyMerchant === null ? this.getXlyAttr('merchant') : xlyMerchant;
-        },
+        function getMerchant() {
+            return 'undefined' === typeof xlyMerchant || xlyMerchant === null ? getXlyAttr('merchant') : xlyMerchant;
+        }
 
-        getEmail: function () {
-            return this.getXlyAttr('cemail');
-        },
+        function getEmail() {
+            return getXlyAttr('cemail');
+        }
 
-        getFullName: function () {
-            return this.getXlyAttr('cname');
-        },
+        function getFullName() {
+            return getXlyAttr('cname');
+        }
 
-        xlyRenderCreative: function (payload, el) {
+        function render(payload, el) {
             var img = el.querySelector("img");
             if (el.href === "%%XLY_LINK%%" || el.href === document.location.href || el.href === '' || !el.href) {
                 el.href = payload.migrationLink;
@@ -98,75 +119,84 @@ var xly = xly || {
                 el.href = el.href + encodeURIComponent(payload.migrationLink);
             }
             img.src = payload.bannerImageUrl;
-        },
+        }
 
-        initialise: function () {
-            var creatives = document.getElementsByClassName("xly-creative");
-            for (var i = 0; i < creatives.length; i++) {
-                var link = creatives[i];
-                if (link.xlyReady) {
-                    continue;
-                }
-                link.xlyReady = true;
+        return {
+            autoinit: function() {
+              return 'false' !== getXlyAttr('autoinit');
+            },
 
-                var img = link.querySelector("img");
-                var ppid = this.getPpid();
-                var merchant = this.getMerchant();
-                var campaigns = link.getAttribute('data-campaigns');
-                campaigns = campaigns ? campaigns.split(",") : null;
-                console.log(campaigns);
-                if (ppid) {
-                    this.callExpressly('', this.xlyRenderCreative, link,
-                        {
-                            ppid: ppid,
-                            width: img.getAttribute("width"),
-                            height: img.getAttribute("height"),
-                            campaigns: campaigns,
-                            source: this.getXlyAttr('source')
-                        }
-                    );
-                } else if (merchant) {
-                    var email = this.getEmail();
-                    var fullName = this.getFullName();
-                    if (email && fullName) {
-                        this.callExpressly('/precache', this.xlyRenderCreative, link,
-                            {
-                                merchantUuid: merchant,
-                                width: img.getAttribute("width"),
-                                height: img.getAttribute("height"),
-                                campaigns: campaigns,
-                                source: this.getXlyAttr('source'),
-                                email:  email,
-                                fullName: fullName,
-                                forename: this.getXlyAttr('cforename'),
-                                surname: this.getXlyAttr('csurname'),
-                                address1: this.getXlyAttr('caddress1'),
-                                address2: this.getXlyAttr('caddress2'),
-                                city: this.getXlyAttr('ccity'),
-                                province: this.getXlyAttr('cprovince'),
-                                zip: this.getXlyAttr('czip'),
-                                country: this.getXlyAttr('ccountry'),
-                                phone: this.getXlyAttr('cphone'),
-                                gender: this.getXlyAttr('cgender'),
-                                dob: this.getXlyAttr('cdob')
-                            }
-                        );
-                    } else {
-                        this.callExpressly('/anonymous', this.xlyRenderCreative, link,
-                            {
-                                merchantUuid: merchant,
-                                width: img.getAttribute("width"),
-                                height: img.getAttribute("height"),
-                                campaigns: campaigns,
-                                source: this.getXlyAttr('source')
-                            }
-                        );
+            initialise: function () {
+                var creatives = document.querySelectorAll(".xly-creative");
+                for (var i = 0; i < creatives.length; i++) {
+                    var link = creatives[i];
+                    if (link.xlyReady) {
+                        continue;
                     }
-                } else {
-                    console.log("meta tag with xly:ppid or xly:merchant not found");
+                    link.xlyReady = true;
+
+                    var img = link.querySelector("img");
+                    var ppid = getPpid();
+                    var merchant = getMerchant();
+                    var campaigns = link.getAttribute('data-campaigns');
+                    campaigns = campaigns ? campaigns.split(",") : null;
+                    if (ppid) {
+                        call('', link,
+                            {
+                                ppid: ppid,
+                                width: img.getAttribute("width"),
+                                height: img.getAttribute("height"),
+                                campaigns: campaigns,
+                                source: getXlyAttr('source')
+                            }
+                        );
+                    } else if (merchant) {
+                        var email = getEmail();
+                        var fullName = getFullName();
+                        if (email && fullName) {
+                            call('/precache', link,
+                                {
+                                    merchantUuid: merchant,
+                                    width: img.getAttribute("width"),
+                                    height: img.getAttribute("height"),
+                                    campaigns: campaigns,
+                                    source: getXlyAttr('source'),
+                                    email: email,
+                                    fullName: fullName,
+                                    forename: getXlyAttr('cforename'),
+                                    surname: getXlyAttr('csurname'),
+                                    address1: getXlyAttr('caddress1'),
+                                    address2: getXlyAttr('caddress2'),
+                                    city: getXlyAttr('ccity'),
+                                    province: getXlyAttr('cprovince'),
+                                    zip: getXlyAttr('czip'),
+                                    country: getXlyAttr('ccountry'),
+                                    phone: getXlyAttr('cphone'),
+                                    gender: getXlyAttr('cgender'),
+                                    dob: getXlyAttr('cdob'),
+                                    expresslyPaymentToken: getXlyAttr('cpaymentToken')
+                                }
+                            );
+                        } else {
+                            call('/anonymous', link,
+                                {
+                                    merchantUuid: merchant,
+                                    width: img.getAttribute("width"),
+                                    height: img.getAttribute("height"),
+                                    campaigns: campaigns,
+                                    source: getXlyAttr('source')
+                                }
+                            );
+                        }
+                    } else {
+                        console.log("meta tag with xly:ppid or xly:merchant not found");
+                    }
                 }
             }
         }
-    };
+        
+    })();
 
-xly.initialise();
+if (xly.autoinit()) {
+    xly.initialise();
+}
